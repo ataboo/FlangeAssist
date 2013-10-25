be507ea1f10478a8569e411252481073b883ab20
ataboo <doubar2001@gmail.com>
2013-10-25 03:32:22 -0600

    Changed theme, adjusted actionbar intent

--- src/com/atasoft/flangeassist/AsmeFlange.java
+++ src/com/atasoft/flangeassist/AsmeFlange.java
@@ -59,7 +59,7 @@ public class AsmeFlange extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
 			case android.R.id.home:
-				NavUtils.navigateUpFromSameTask(this);
+				super.finish();
 				return true;
 			default:
 				return super.onOptionsItemSelected(item);
