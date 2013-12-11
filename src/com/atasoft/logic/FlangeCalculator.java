f1d46436b1b97713a3635bba36dac03848946288
ataboo <doubar2001@gmail.com>
2013-12-10 22:44:23 -0700

    prep to branch back to main

---
+++ src/com/atasoft/logic/FlangeCalculator.java
@@ -0,0 +1,11 @@
+package com.atasoft.logic;
+import com.atasoft.flangeassist.*;
+
+public class FlangeCalculator
+{
+	private FlangeCalculator flangeFrag;
+	
+	public FlangeCalculator(FlangeFragment flangeFrag) {
+		
+	}
+}
