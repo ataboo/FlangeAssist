3a9528f6b1a966bdfe83bf29bc94197e86ebf48b
Alex Raboud <bmtoolbox@gmail.com>
2014-05-28 06:11:40 -0600

    Started using Hashmaps in Converter

--- src/com/atasoft/helpers/ConvDataHold.java
+++ src/com/atasoft/helpers/ConvDataHold.java
@@ -1,5 +1,5 @@
 package com.atasoft.helpers;
-import android.nfc.*;
+import java.util.*;
 
 //Keeps the figures and tables for the Unit Converter in Boilermaker Toolbox
 public class ConvDataHold
@@ -15,8 +15,24 @@ public class ConvDataHold
 	private static final double[] massRates = {1d, 2.2d, 4200d, 4400d};
 	private static final String[] volumeStrings = {"Litres", "Gallons", "Cubic Feet"};
 	private static final double[] volumeRates = {1d, 3.2d, 0.5d};
-	
+	private HashMap<String, Double> lengthHash;
+	private HashMap<String, Double> forceHash;
+	private HashMap<String, Double> massHash;
+	private HashMap<String, Double> volumeHash;
 	public ConvDataHold() {
+		lengthHash = makeHash(lengthStrings, lengthRates);
+		forceHash = makeHash(forceStrings, forceRates);
+		massHash = makeHash(massStrings, massRates);
+		volumeHash = makeHash(volumeStrings, volumeRates);
+		
+	}
+	
+	private HashMap makeHash(String[] keys, double[] doubs){
+		HashMap<String, Double> retHash = new HashMap<String, Double>(keys.length);
+		for(int i = 0; i<keys.length; i++){
+			retHash.put(keys[i], doubs[i]);
+		}
+		return retHash;
 	}
 	
 	public String[] getUnitStrings(int typeConst) {
